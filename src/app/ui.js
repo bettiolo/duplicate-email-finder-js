40ecb342a2b79b9e3a732963f9097b9b53792659
Marco Bettiolo <marco.bettiolo@7digital.com>
2013-11-18 12:14:07 +0000

    splitting up rendering logic

--- src/app/ui.js
+++ src/app/ui.js
@@ -2,10 +2,29 @@ $(function () {
 	'use strict';
 
 	$('#parse-data').click(function () {
-		findDuplicates($('#source-data').text(), $('#output'));
+		var inputText = $('#source-data').val(),
+			duplicateCount = 0,
+			uniqueCount = 0;
+		findDuplicates(inputText, $('#duplicate-output'), function matchDuplicateOnly(duplicate) {
+			if (duplicate.length > 1) {
+				duplicateCount++;
+				return true;
+			}
+			return false;
+		});
+		findDuplicates(inputText, $('#unique-output'), function matchUniqueOnly(duplicate) {
+			if (duplicate.length == 1) {
+				uniqueCount++;
+				return true;
+			}
+			return false;
+		});
+		$('#duplicate-count').text(duplicateCount);
+		$('#unique-count').text(uniqueCount);
 	});
 
-	function findDuplicates(inputText, $output) {
+	function findDuplicates(inputText, $output, matcher) {
+		$output.empty();
 		var duplicates = duplicateEmails.find(inputText),
 			duplicateAddresses = Object.keys(duplicates),
 			$duplicatesList = $('<ul></ul>'),
@@ -14,7 +33,9 @@ $(function () {
 
 		for (i = 0; i < duplicateAddresses.length; i++) {
 			address = duplicateAddresses[i];
-			$duplicatesList.append(getDuplicateOutputElement(address, duplicates[address]));
+			if (matcher(duplicates[address])) {
+				$duplicatesList.append(getDuplicateOutputElement(address, duplicates[address]));
+			}
 		}
 
 		$output.append($duplicatesList);
@@ -26,18 +47,31 @@ $(function () {
 			$duplicateEntry,
 			$addressCell,
 			i;
-		$duplicateListItem.text(address);
+		$duplicateListItem.text(address + ' (' + duplicate.length + ')');
 		if (duplicate) {
 			for (i = 0; i < duplicate.length; i++) {
 				$duplicateEntry = $('<tr></tr>');
 				$addressCell = $('<td></td>');
-				$addressCell.text(duplicate[i].address.filtered);
+				$addressCell.text(duplicate[i].address.original);
 				$duplicateEntry.append($addressCell);
+				renderDuplicateEntries($duplicateEntry, duplicate[i]);
 				$duplicateEntries.append($duplicateEntry);
 			}
 		}
 		$duplicateListItem.append($duplicateEntries);
 		return $duplicateListItem;
+	}
+
+	function renderDuplicateEntries($duplicateEntry, duplicateEntries) {
+		var $fieldCell,
+			field,
+			i;
+		for (i = 0; i < duplicateEntries.fields.length; i++) {
+			field = duplicateEntries.fields[i];
+			$fieldCell = $('<td></td>');
+			$fieldCell.text(field);
+			$duplicateEntry.append($fieldCell);
+		}
 	}
 
 });
