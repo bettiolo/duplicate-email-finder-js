583564b70139ce03451aea68af52cf1274cd2d9e
Marco Bettiolo <marco.bettiolo@7digital.com>
2013-11-18 14:17:11 +0000

    Now rendering a table

--- src/app/ui.js
+++ src/app/ui.js
@@ -5,15 +5,16 @@ $(function () {
 		var inputText = $('#source-data').val(),
 			duplicates = duplicateEmails.find(inputText),
 			duplicateCount = 0,
-			uniqueCount = 0;
-		renderDuplicates(duplicates, $('#duplicate-output'), function matchDuplicateOnly(duplicate) {
+			uniqueCount = 0,
+			maxFieldCount = getFieldCount(duplicates);
+		renderDuplicates(duplicates, maxFieldCount, $('#duplicate-output'), function matchDuplicateOnly(duplicate) {
 			if (duplicate.length > 1) {
 				duplicateCount++;
 				return true;
 			}
 			return false;
 		});
-		renderDuplicates(duplicates, $('#unique-output'), function matchUniqueOnly(duplicate) {
+		renderDuplicates(duplicates, maxFieldCount, $('#unique-output'), function matchUniqueOnly(duplicate) {
 			if (duplicate.length == 1) {
 				uniqueCount++;
 				return true;
@@ -24,7 +25,33 @@ $(function () {
 		$('#unique-count').text(uniqueCount);
 	});
 
-	function renderDuplicates(duplicates, $output, matcher) {
+	function getFieldCount(duplicates) {
+		var duplicateAddresses = Object.keys(duplicates),
+			fieldCount,
+			maxFieldCount = 0,
+			duplicate,
+			duplicateIndex,
+			entry,
+			entryIndex,
+			fieldIndex;
+
+		for (duplicateIndex = 0; duplicateIndex < duplicateAddresses.length; duplicateIndex++) {
+			duplicate = duplicates[duplicateAddresses[duplicateIndex]];
+			for (entryIndex = 0; entryIndex < duplicate.length; entryIndex++) {
+				entry = duplicate[entryIndex];
+				fieldCount = 1; // address
+				for (fieldIndex = 0; fieldIndex < entry.fields.length; fieldIndex++) {
+					fieldCount ++;
+				}
+				if (fieldCount > maxFieldCount) {
+					maxFieldCount = fieldCount;
+				}
+			}
+		}
+		return maxFieldCount;
+	}
+
+	function renderDuplicates(duplicates, maxFieldCount, $output, matcher) {
 		$output.empty();
 		var duplicateAddresses = Object.keys(duplicates),
 			$duplicates = $('<table></table>'),
@@ -34,16 +61,17 @@ $(function () {
 		for (i = 0; i < duplicateAddresses.length; i++) {
 			address = duplicateAddresses[i];
 			if (matcher(duplicates[address])) {
-				$duplicates.append(renderDuplicate(address, duplicates[address]));
+				$duplicates.append(renderDuplicate(address, duplicates[address], maxFieldCount));
 			}
 		}
 		$output.append($duplicates);
 	}
 
-	function renderDuplicate(address, duplicate) {
+	function renderDuplicate(address, duplicate, maxFieldCount) {
 		var $duplicate = $('<tbody></tbody>'),
 			$duplicateRow = $('<tr></tr>'),
-			$duplicateItem = $('<th></th>'),
+			$duplicateItem = $('<th colspan="' + (maxFieldCount + 1) + '"></th>'),
+			$separator = $('<tr><td colspan="' + (maxFieldCount + 1) + '">&nbsp;</td></tr>'),
 			i;
 
 		$duplicateItem.text(address + ' (' + duplicate.length + ')');
@@ -52,16 +80,19 @@ $(function () {
 		for (i = 0; i < duplicate.length; i++) {
 			$duplicate.append(renderDuplicateEntries(duplicate[i]));
 		}
+		$duplicate.append($separator);
 		return $duplicate;
 	}
 
 	function renderDuplicateEntries(duplicateEntries) {
 		var $duplicateEntry = $('<tr></tr>'),
+			$rowHeaderCell = $('<td>-</td>'),
 			$addressCell = $('<td></td>'),
 			$fieldCell,
 			field,
 			i;
 
+		$duplicateEntry.append($rowHeaderCell);
 		$addressCell.text(duplicateEntries.address.original);
 		$duplicateEntry.append($addressCell);
 		for (i = 0; i < duplicateEntries.fields.length; i++) {
